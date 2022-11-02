5ca3fc46087a87819370c2ea16e56eb5574acc3b
Evgeniy Budaev <budaev.e@gmail.com>
2022-11-02 08:18:23 +0300

    init

---
+++ src/main/java/com/example/spring_course_zaur/SpringCourseZaurApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_course_zaur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseZaurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseZaurApplication.class, args);
+	}
+
+}
